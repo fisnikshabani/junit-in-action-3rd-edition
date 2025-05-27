451fdf465213ef30d4d88fabca49363fb6bc3ab3
Fisnik Shabani <fisnik.shabani@endava.com>
2025-05-27 10:49:03 +0200

    chapter 3: custom rules

---
+++ ch03-architecture/src/test/java/com/manning/junitbook/rules/CustomRuleTester.java
@@ -0,0 +1,15 @@
+package com.manning.junitbook.rules;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class CustomRuleTester {
+
+    @Rule
+    public CustomRule myRule = new CustomRule();
+
+    @Test
+    public void myCustomRuleTest() {
+        System.out.println("Call of a test method");
+    }
+}
